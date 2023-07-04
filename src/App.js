ad996f2dd4789cd7344de29ab1c411415601997f
yash <yash@HP-340S-G7-Notebook-PC-4ea08bb8.localdomain>
2023-07-04 16:30:41 +0530

    changed gtm tag

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import { CommonContext, CommonProvider } from './contexts/CommonContext';
 
 import AnimatedRoutes from './components/AnimatedRoutes';
 import { AuthContextProvider } from './contexts/AuthContext';
+import TagManager from 'react-gtm-module'
+import { useEffect } from 'react';
 
 function App() {
   
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: 'GTM-WMJMQ9M'
+    })
+  }, [])
 
   return (
     <CommonProvider>
--- src/index.js
+++ src/index.js
@@ -6,17 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { App as CapApp } from '@capacitor/app';
 import ScrollToTop from './components/ScrollToTop';
-import TagManager from 'react-gtm-module'
 
 
 CapApp.addListener('backButton', ({ canGoBack }) => {
 
   const urlArray = window.location.href.split("/")
-
-  const tagManagerArgs = {
-    gtmId: 'GTM-WMJMQ9M'
-  }
-  TagManager.initialize(tagManagerArgs)
 
   // if (urlArray[urlArray.length - 1] === 'auth') {
   //   CapApp.exitApp();
